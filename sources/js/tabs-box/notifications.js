4196ff7f35152670962109c3354703a6ad510919
Sergey_Mironov <sergeymironov0001@gmail.com>
2018-01-07 00:32:18 +0300

    Refactor notifications.js.

--- sources/js/tabs-box/notifications.js
+++ sources/js/tabs-box/notifications.js
@@ -1,146 +1,128 @@
-function Notifications() {
-}
+class Notifications {
 
-function sendMessage(message, callback) {
-    chrome.runtime.sendMessage(message, function (response) {
-        if (callback) {
-            callback(response);
-        }
-    });
-}
+    static sendNewBoxAdded(box, callback) {
+        Notifications._sendMessage({
+                type: "tabs-box:new-box-added",
+                box: box
+            },
+            callback);
+    }
 
-Notifications.sendNewBoxAdded = function (box, callback) {
-    sendMessage({
-            type: "tabs-box:new-box-added",
-            box: box
-        },
-        callback);
-};
+    static sendBoxNameChanged(box) {
+        Notifications._sendMessage({
+            type: "tabs-box:change-box-name",
+            id: box.id,
+            name: box.name
+        });
+    }
 
-Notifications.sendBoxNameChanged = function (box) {
-    sendMessage({
-        type: "tabs-box:change-box-name",
-        id: box.id,
-        name: box.name
-    });
-};
+    static sendPutTabToBox(boxId, tab, callback) {
+        Notifications._sendMessage({
+                type: "tabs-box:put-tab-to-box",
+                boxId: boxId,
+                tab: tab
+            },
+            callback);
+    }
 
-Notifications.sendPutTabToBox = function (boxId, tab, callback) {
-    sendMessage({
-            type: "tabs-box:put-tab-to-box",
+    static sendChangeTabPosition(boxId, tabId, newPosition, callback) {
+        Notifications._sendMessage({
+            type: "tabs-box:change-tab-position",
             boxId: boxId,
-            tab: tab
-        },
-        callback);
-};
+            tabId: tabId,
+            newPosition: newPosition
+        }, callback);
+    }
 
-Notifications.sendTabForBoxOpened = function (callback) {
-    sendMessage({
-        type: "tabs-box:tab-for-box-opened"
-    }, callback);
-};
+    static sendTabFromBoxRemoved(boxId, tab, callback) {
+        Notifications._sendMessage({
+                type: "tabs-box:remove-tab-from-box",
+                boxId: boxId,
+                tab: tab
+            },
+            callback);
+    }
 
-Notifications.sendChangeTabPosition = function (boxId, tabId, newPosition, callback) {
-    sendMessage({
-        type: "tabs-box:change-tab-position",
-        boxId: boxId,
-        tabId: tabId,
-        newPosition: newPosition
-    }, callback);
-};
+    static sendBoxRemoved(box, callback) {
+        Notifications._sendMessage({
+                type: "tabs-box:box-removed",
+                box: box
+            },
+            callback);
+    }
 
-Notifications.sendTabFromBoxRemoved = function (boxId, tab, callback) {
-    sendMessage({
-            type: "tabs-box:remove-tab-from-box",
-            boxId: boxId,
-            tab: tab
-        },
-        callback);
-};
-
-Notifications.sendBoxRemoved = function (box, callback) {
-    sendMessage({
-            type: "tabs-box:box-removed",
-            box: box
-        },
-        callback);
-};
-
-function addListener(requestType, callback) {
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        if (requestType === request.type) {
-            callback(request);
-        }
-    });
-}
-
-Notifications.addNewBoxAddedListener = function (callback) {
-    addListener("tabs-box:new-box-added", function (request) {
-        callback(request.box);
-    });
-};
+    static addNewBoxAddedListener(callback) {
+        Notifications._addListener("tabs-box:new-box-added", function (request) {
+            callback(request.box);
+        });
+    }
 
 
-Notifications.addChangeBoxNameListener = function (boxId, callback) {
-    addListener("tabs-box:change-box-name", function (request) {
-        if (request.id === boxId) {
-            callback(request.name);
-        }
-    });
-};
+    static addChangeBoxNameListener(boxId, callback) {
+        Notifications._addListener("tabs-box:change-box-name", function (request) {
+            if (request.id === boxId) {
+                callback(request.name);
+            }
+        });
+    }
 
-Notifications.addChangeBoxNameListener1 = function (callback) {
-    addListener("tabs-box:change-box-name", function (request) {
-        callback(request.id, request.name);
-    });
-};
+    static addRemoveTabFromBoxListener(boxId, callback) {
+        Notifications._addListener("tabs-box:remove-tab-from-box", function (request) {
+                if (boxId === request.boxId) {
+                    callback(request.tab);
+                }
+            }
+        );
+    }
 
-Notifications.addRemoveTabFromBoxListener = function (callback) {
-    addListener("tabs-box:remove-tab-from-box", function (request) {
-        callback(request.boxId, request.tab);
-    });
-};
-
-
-Notifications.addRemoveTabFromBoxListener = function (boxId, callback) {
-    addListener("tabs-box:remove-tab-from-box", function (request) {
+    static addPutTabToBoxListener(boxId, callback) {
+        Notifications._addListener("tabs-box:put-tab-to-box", function (request) {
             if (boxId === request.boxId) {
                 callback(request.tab);
             }
-        }
-    );
-};
+        });
+    }
 
-Notifications.addPutTabToBoxListener = function (boxId, callback) {
-    addListener("tabs-box:put-tab-to-box", function (request) {
-        if (boxId === request.boxId) {
-            callback(request.tab);
-        }
-    });
-};
+    static addBoxRemovedListener(callback) {
+        Notifications._addListener("tabs-box:box-removed", function (request) {
+            callback(request.box)
+        });
+    }
 
-Notifications.addBoxRemovedListener = function (callback) {
-    addListener("tabs-box:box-removed", function (request) {
-        callback(request.box)
-    });
-};
+    static addTabRemoveListener(callback) {
+        chrome.tabs.onRemoved.addListener(function (tabId, removed) {
+            callback(tabId);
+        });
+    }
 
-Notifications.addTabRemoveListener = function (callback) {
-    chrome.tabs.onRemoved.addListener(function (tabId, removed) {
-        callback(tabId);
-    });
-};
+    static addChangeTabPositionListener(boxId, callback) {
+        Notifications._addListener("tabs-box:change-tab-position", function (request) {
+            if (boxId === request.boxId) {
+                callback(request.tabId, request.newPosition);
+            }
+        });
+    }
 
-Notifications.addChangeTabPositionListener = function (boxId, callback) {
-    addListener("tabs-box:change-tab-position", function (request) {
-        if (boxId === request.boxId) {
-            callback(request.tabId, request.newPosition);
-        }
-    });
-};
+    static addChangeTabsPositionListener(callback) {
+        Notifications._addListener("tabs-box:change-tab-position", function (request) {
+            callback(request.boxId, request.tabId, request.newPosition);
+        });
+    }
 
-Notifications.addChangeTabsPositionListener = function (callback) {
-    addListener("tabs-box:change-tab-position", function (request) {
-        callback(request.boxId, request.tabId, request.newPosition);
-    });
-};
+
+    static _sendMessage(message, callback) {
+        chrome.runtime.sendMessage(message, function (response) {
+            if (callback) {
+                callback(response);
+            }
+        });
+    }
+
+    static _addListener(requestType, callback) {
+        chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+            if (requestType === request.type) {
+                callback(request);
+            }
+        });
+    }
+}
