2ebca11c7e6e0c76deb38f7c1901572543d15f9b
Sergey_Mironov <sergeymironov0001@gmail.com>
2018-01-07 01:04:32 +0300

    Refactor templates.js.

--- sources/js/tabs-box/templates.js
+++ sources/js/tabs-box/templates.js
@@ -1,19 +1,18 @@
-function Templates() {
+class Templates {
+    static loadPopupBoxTemplate(callback) {
+        Templates._loadTemplate("../templates/popup-box.html", callback);
+    };
+
+    static loadTabSnapshotTemplate(callback) {
+        Templates._loadTemplate("../templates/tab-snapshot.html", callback);
+    };
+
+    static _loadTemplate(templateName, callback) {
+        $.get(chrome.extension.getURL(templateName), function (template) {
+            Mustache.parse(template);
+            callback(template);
+        });
+    }
 }
-
-function loadTemplate(templateName, callback) {
-    $.get(chrome.extension.getURL(templateName), function (template) {
-        Mustache.parse(template);
-        callback(template);
-    });
-}
-
-Templates.loadPopupBoxTemplate = function (callback) {
-    loadTemplate("../templates/popup-box.html", callback);
-};
-
-Templates.loadTabSnapshotTemplate = function (callback) {
-    loadTemplate("../templates/tab-snapshot.html", callback);
-};
 
 
